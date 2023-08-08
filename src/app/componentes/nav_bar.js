aa784ae051d38a8b9fd79859d5b33d895fec8f8c
JulietaGT <108838724+JulietaGT@users.noreply.github.com>
2023-08-08 09:11:31 -0300

    cree un nuevo componente para la barra de navegacion

---
+++ src/app/componentes/nav_bar.js
@@ -0,0 +1,14 @@
+import { Bubblegum_Sans } from 'next/font/google';
+import React from 'react';
+
+const Barra = () => {
+   const styles = {
+    backgroundColor:'lightblue',
+    color: 'green',
+   };
+    return (
+        <nav style={styles}></nav>
+    );
+};
+
+export default Barra;
