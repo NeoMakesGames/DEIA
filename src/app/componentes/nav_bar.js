0b0198a7c1df8ca932e1e4fb733896e30a90ab94
JulietaGT <108838724+JulietaGT@users.noreply.github.com>
2023-08-10 15:54:50 -0300

    navbar con boton
    agregue una barra e hice un boton de log in basico

--- src/app/componentes/nav_bar.js
+++ src/app/componentes/nav_bar.js
@@ -1,4 +1,12 @@
 import { Bubblegum_Sans } from 'next/font/google';
 import React from 'react';
 
-export default Barra;
+export default function Barra(){
+    return (
+    <nav class="barra">
+        <div>
+        <span class="self-center text-2xl font-semibold whitespace-nowrap txt-color-amber">DEIA</span>
+        </div>
+        <button class="bg-amber-300 hover:bg-amber-400 text-white py-3 px-3 rounded-md m-2  ">Log in</button>
+    </nav>
+)}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-//import Barra from './componentes/nav_bar'
+import Barra from './componentes/nav_bar'
 import Auth from "./Auth"
 
 export default function Home() {
   return (
    <div>
-      <h1 className="border-2 border-black">Deia</h1>
-      
+     <Barra />
+     <h1>Deia</h1>
    </div>
   )
 }
