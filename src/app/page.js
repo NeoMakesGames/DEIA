2998a3f4368b45072d264c99678b02e3b65062ff
BdeBarbara <benjadbf@gmail.com>
2023-08-15 16:41:55 -0300

    -+

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,6 @@
 import Barra from './componentes/nav_bar'
 import Contenido from './componentes/content.pag.principal'
 
-import Auth from "./Auth"
-
 export default function Home() {
   return (
    <div>
