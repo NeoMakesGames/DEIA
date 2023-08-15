6913a62178bd37f48ff94eacbe237d875bee3294
BdeBarbara <benjadbf@gmail.com>
2023-08-15 16:19:47 -0300

    +requiered form register

--- web/src/app/hooks/Register.js
+++ web/src/app/hooks/Register.js
@@ -5,24 +5,26 @@ import { useForm, SubmitHandler } from "react-hook-form"
 
   export default function RegisterForm() {
 
-    const {register, handleSubmit} = useForm( );
+    const {register, handleSubmit, watch, formState: {errors}} = useForm(Inputs);
 
     async function reg (data){
         console.log(data);
         const record = await pb.collection("users").create(data);
         console.log(pb.authStore.isValid);
+
+
     }
 
     return (
 
     <>
     <form onSubmit={handleSubmit(reg)}>
-        <label htmlFor="logIn-name"> Usuario: <input id='logIn-username' type='text' placeholder='Nombre' {...register("username")}/> </label>
-        <label htmlFor="logIn-name"> Nombre: <input id='logIn-name' type='text' placeholder='Nombre' {...register("name")}/> </label>
-        <label htmlFor="logIn-surName"> Apellido: <input id='logIn-surName' type='text' placeholder='Apellido' {...register("surName")}/> </label>
-        <label htmlFor="logIn-mail"> Mail: <input id='logIn-mail' type="email" placeholder='Mail' {...register("email")}/> </label>
-        <label htmlFor="logIn-pass"> Contraseña: <input id='logIn-pass' type='password' placeholder='********' {...register("password")}/> </label>
-        <label htmlFor="logIn-passCo"> Confirmar Contraseña: <input id='logIn-passCo' type='password' placeholder='********' {...register("passwordConfirm")}/> </label>
+        <label htmlFor="logIn-name"> Usuario: <input id='logIn-username' type='text' placeholder='Nombre' {...register("username"), { required: "Name is required", min: 1, max: 32}}aria-invalid={errors.username ? true:false}/> </label>
+        <label htmlFor="logIn-name"> Nombre: <input id='logIn-name' type='text' placeholder='Nombre' {...register("name"), { required: true, min:1, max: 32 }}/> </label>
+        <label htmlFor="logIn-surName"> Apellido: <input id='logIn-surName' type='text' placeholder='Apellido' {...register("surName"), { required: true, min:1, max:48 }}/> </label>
+        <label htmlFor="logIn-mail"> Mail: <input id='logIn-mail' type="email" placeholder='Mail' {...register("email"), { required: true, min:1, max: 64 }}/> </label>
+        <label htmlFor="logIn-pass"> Contraseña: <input id='logIn-pass' type='password' placeholder='********' {...register("password"), { required: true, min:10, max: 64 }}/> </label>
+        <label htmlFor="logIn-passCo"> Confirmar Contraseña: <input id='logIn-passCo' type='password' placeholder='********' {...register("passwordConfirm"), { required: true, min:10, max:64}}/> </label>
         <button type='submit'>Registrarse</button>
     </form>
     </>
