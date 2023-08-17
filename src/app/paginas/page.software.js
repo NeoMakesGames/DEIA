1a7dc3e21e71a996b69ed5c260a0fbc3fded7639
JulietaGT <julietaguerson@gmail.com>
2023-08-17 18:25:42 -0300

    react routes
    añadi un elemento de react llamado routes para hacer los cambios de paginas

--- src/app/paginas/page.software.js
+++ src/app/paginas/page.software.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Barra from "./componentes/nav_bar";
+
 export default function Software() {
     return (
      <div>
-       <Barra/>
+       
       <button class="btn btn-secondary">Boton</button>
      </div>
     )
