f04df314a24f90b95d4b27089c9ea9d62cce30b4
JulietaGT <108838724+JulietaGT@users.noreply.github.com>
2023-08-29 09:45:13 -0300

    Update index.js

--- deia/src/pages/index.js
+++ deia/src/pages/index.js
@@ -9,6 +9,15 @@ export default function Home() {
   return (
     <main>
       <Navbar/>
+      <div class="hero min-h-screen bg-base-200">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">DEIA</h1>
+      <p class="py-6">Continuamos generando este proyecto, pero en unos meses va a ser mejor.</p>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
     </main>
 
 
