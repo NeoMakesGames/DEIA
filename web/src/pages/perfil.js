17702960b4d7ac7fb19e03d0cd5aa6b9fbf146eb
JulietaGT <108838724+JulietaGT@users.noreply.github.com>
2023-09-04 10:12:12 -0300

    Update perfil.js

--- web/src/pages/perfil.js
+++ web/src/pages/perfil.js
@@ -5,7 +5,7 @@ export default function Perfil(){
             <Navbar/>
             {/* <h1></h1> */}
             <div className="flex w-full p-4">
-  <container className="grid h-4/5 md:h-full flex-grow card bg-base-300 rounded-box ">
+  <container className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">
       {/* colocar aca los elemento que se mostrarian los elementos ingresados en el login */}
     
         {/* Titulo y foto de perfil del paciente/doctor */}
@@ -14,8 +14,6 @@ export default function Perfil(){
 
       </container>
 
-
-  <div className="divider divider-horizontal"></div>
   <div className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">historial</div>
 </div>
         </main>
