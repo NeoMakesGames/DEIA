d9cdcd733f4d3749f3db3dd354bbf47f9c40d9d1
JulietaGT <julietaguerson@gmail.com>
2023-09-07 18:48:37 -0300

    hembelleci al login

--- web/src/pages/login.js
+++ web/src/pages/login.js
@@ -18,18 +18,18 @@ export default function Login(){
     return(
         <main>
             <Navbar/>
-                <div  className="place-content-center bg-current">
+                <div className=" w-screen flex  h-screen bg-gradient-to-r from-secondary-900 to-secondary-100 ">
                     
-                    <div  >
-                        <form onSubmit={handleSubmit(reg)} className="grid grid-col-1 place-content-left bg-primary w-2/5">
-                            <h1 className="p-4 ">Login</h1>
-                            <label className="p-4 w-full max-w-2xl bg-transparent" htmlFor="logIn-name"> Usuario: <br/> <input id='logIn-username' type='text' placeholder='Nombre' className="input p-1 w-2/3" {...register("username")}/> </label>
-                            <label className="p-4 w-full max-w-2xl bg-transparent" htmlFor="logIn-name"> Nombre: <br/><input id='logIn-name' type='text' placeholder='Nombre' className="input p-1 w-2/3"{...register("name")}/> </label>
-                            <label className="p-4 w-full max-w-2xl bg-transparent" htmlFor="logIn-surName"> Apellido: <br/><input id='logIn-surName' type='text' placeholder='Apellido' className="input p-1 w-2/3"{...register("surName")}/> </label>
-                            <label className="p-4 w-full max-w-2xl bg-transparent" htmlFor="logIn-mail"> Mail: <br/><input id='logIn-mail' type="email" placeholder='Mail' className="input p-1 w-2/3"{...register("email")}/> </label>
-                            <label className="p-4 w-full max-w-2xl bg-transparent" htmlFor="logIn-pass"> Contraseña: <br/><input id='logIn-pass' type='password' placeholder='********' className="input p-1 w-2/3"{...register("password")}/> </label>
-                            <label className="p-4 w-full max-w-2xl bg-transparent " htmlFor="logIn-passCo"> Confirmar Contraseña: <br/><input id='logIn-passCo' type='password' placeholder='********' className="input p-1 w-2/3"{...register("passwordConfirm")}/> </label>
-                            <button type='submit' className="btn max-w-xs p-4">Registrarse</button>
+                    <div className=" bg-neutral flex justify-center flex-auto mx-36" >
+                        <form onSubmit={handleSubmit(reg)} className="grid grid-col-1 place-items-center justify-self-center grow md:grow-0 text-primary">
+                            <h1 className="p-6 font-mono font-bold text-4xl text-center text-primary ">Sign up</h1>
+                            <label htmlFor="logIn-name"> Usuario: <br/> <input id='logIn-username' type='text' placeholder='Nombre' className="input p-1 max-w-fit " {...register("username")}/> </label>
+                            <label htmlFor="logIn-name"> Nombre: <br/><input id='logIn-name' type='text' placeholder='Nombre' className="input p-1 w-fit"{...register("name")}/> </label>
+                            <label htmlFor="logIn-surName"> Apellido: <br/><input id='logIn-surName' type='text' placeholder='Apellido' className="input p-1 w-fit"{...register("surName")}/> </label>
+                            <label htmlFor="logIn-mail"> Mail: <br/><input id='logIn-mail' type="email" placeholder='Mail' className="input p-1 w-fit"{...register("email")}/> </label>
+                            <label htmlFor="logIn-pass"> Contraseña: <br/><input id='logIn-pass' type='password' placeholder='********' className="input p-1 w-fit"{...register("password")}/> </label>
+                            <label htmlFor="logIn-passCo"> Confirmar Contraseña: <br/><input id='logIn-passCo' type='password' placeholder='********' className="input p-1 w-fit"{...register("passwordConfirm")}/> </label>
+                            <button type='submit' className="btn btn-lg m-4">Registrarse</button>
                         </form>
                     </div>
                 </div>
