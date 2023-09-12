ab7cb6416a99d5d9563a26ade4a6651fb3d173a7
JulietaGT <108838724+JulietaGT@users.noreply.github.com>
2023-09-12 17:13:21 -0300

    hice mas pruebas
    logre hacer que un boton agregara div

--- web/src/pages/propsPrueba.js
+++ web/src/pages/propsPrueba.js
@@ -1,9 +1,8 @@
-
 import Navbar from "@/components/navbar";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-export default function formPacientes(){
-    /* DICCIONARIO
+export default function formPacientes() {
+  /* DICCIONARIO
     pacientes = {
         datosPaciente: {
             Nombre: nombre
@@ -15,103 +14,118 @@ export default function formPacientes(){
         }
     }
 */
-    const [formData, setFormData] = useState({
-        nombre: '',
-        apellido: '',
-        edad: ''
-    });
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('datos del formulario' , formData);
-    }  ;
-    
-    //  let pacientes = [];
-    //  function historial(event){
+ /* const [nombre, setNombre] = useState('Ingrese nombre');
+  const [apellido, setApellido] = useState('Ingrese apellido');
+  const [edad, setEdad] = useState('Ingrese edad');
 
+  function historial(event) {
+    event.preventDefault();
+    const form = event.target;
 
-    //     event.preventDefault();
-    //      const form = event.target;
+    const Nombre = form.nombre.value;
+    const Apellido = form.apellido.value;
+    const Edad = parseInt(form.edad.value);
+*/
+const [formData, setFormData] = useState({
+  nombre: '',
+  apellido: '',
+  edad: '',
+});
+const [dataArray, setDataArray] = useState([]);
+const handleChange = (e) => {
+  const {name,value} = e.target;
+  setFormData({
+    ...formData,
+    [name]: value,
+  });
+  setFormData({
+    nombre: '',
+    apellido: '',
+    edad: '',
+  });
+};
+const handleSubmit = (e) => {
+  e.preventDefault(); //crea una copia del dataArray
+  setDataArray ([...dataArray, formData]);
+}
+const [divCount, setDivCount] = useState(0); // State to keep track of the number of divs
+const [divList, setDivList] = useState([]);   // State to store the div elements
 
-    //  const Nombre = (form.nombre.value);
-    //  const Apellido = (form.apellido.value);
-    //  const Edad = parseInt(form.edad.value);
-        
-    //  // pacientes.push={Nombre: nombre, Apellido: apellido, Edad: edad};
-    //   console.log(Nombre);
-    //     pacientes.push(paciente);
-    //  let array = [];
-    //  Object.keys(pacientes).forEach(key => {
-    //     let ver =  `${key}` + ": " + "Nombres:" + `${pacientes[key].Nombre}`;
-    //  });
-    //  array.push(ver);
-    //  console.log(array);
-    
-// export default function Prueba() {
-//   let [formData, setFormData] = useState({
-//     nombre: "",
-//     apodo: "",
-//     edad: "",
-//   });
-
-   const handleChange = (e) => {
-     const { name, value } = e.target;
-     setFormData((prevData) => ({
-       ...prevData,
-       [name]: value,
-     }));
-     
-   };
-
-//     // Convert the formData object to an array
-//     let formDataArray = Object.values(formData);
-//     console.log(formDataArray); // This will show the array in the console
-
-//     let dataItems = formDataArray.map((entrada) => <li>
-//         {entrada}
-//     </li>
-//     );
-
-//   };
+// Function to add a new div to the list
+const addDiv = () => { // una key es una variable que almacena un array index
+  const newDiv = <div key={divCount}>New Div {divCount + 1}</div>; // You can customize the content of the div as needed
+  setDivList([...divList, newDiv]);
+  setDivCount(divCount + 1);}
+    // pacientes.push = { Nombre: nombre, Apellido: apellido, Edad: edad };
+    // console.log(Nombre);
+    // pacientes.push(paciente);
+    // let array = [];
+    // Object.keys(pacientes).forEach((key) => {
+    //   // por cada paciente, createState() para 
+    //   let ver = `${key}` + ": " + "Nombres:" + `${pacientes[key].Nombre}` + "Apellido: "+ `${pacientes[key].Apellido}` + "Edad: "+ `${pacientes[key].Edad}`;
+    // });
+    // array.push(ver);
+    // console.log(array);
+  
 
   return (
     <main>
-        <Navbar/>
-      <form className="p-4" name="formulario" onSubmit={handleChange} >
+      <Navbar />
+      <form className="p-4" name="formulario" >
         <div className="p-4 m-4">
-          <input type="text" name="nombre" placeholder="nombre" className="input bg-primary  datoInput"
-          value={formData.nombre}
-          onChange={handleChange}
+          <input
+            type="text"
+            name="nombre"
+            placeholder="nombre"
+            className="input bg-primary  datoInput"
+            value={formData.nombre}
+            onChange={handleChange}
           />
         </div>
         <div className="p-4 m-4">
-          <input type="text" name="apellido" placeholder="apellido" className="input bg-primary datoInput"
-          value={formData.apellido}
-          onChange={handleChange}
+          <input
+            type="text"
+            name="apellido"
+            placeholder="apellido"
+            className="input bg-primary datoInput"
+            value={formData.apellido}
+            onChange={handleChange}
           />
         </div>
         <div className="p-4 m-4">
-          <input type="text" name="edad" placeholder="edad" className="input bg-primary  datoInput"
-          value={formData.edad}
-          onChange={handleChange}
+          <input
+            type="text"
+            name="edad"
+            placeholder="edad"
+            className="input bg-primary  datoInput"
+            value={formData.edad}
+            onChange={handleChange}
           />
         </div>
         <button type="submit" className="btn">
           Submit
         </button>
       </form>
-        <div className="card bg-neutral-500 ">
-            <div className="card-body">
-                <h2>Lista de elementos</h2>
-                <ul>
-                  
-                </ul>
-            </div>
-            
+      <div className="card bg-neutral-500 ">
+        <div className="card-body">
+          <h2>Lista de elementos</h2>
+          <button onClick={addDiv}>click</button> 
+          <ul>
+          {dataArray.map((data, index) => (
+            <li key={index}>
+              Nombre: {data.nombre}, Apellido: {data.apellido}, Edad: {data.edad}
+            </li>
+              ))}
 
+            </ul>
+          {/* /* {divList.map((div, index) => (
+          <li key={index}>{div}</li>
+        ))} */}
+
+        
         </div>
-
-
+      </div>
     </main>
-  )
-};
+  );
+}
