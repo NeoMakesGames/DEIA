4c18b7c2decdfbce16b25d3edb3c26f100482687
JulietaGT <108838724+JulietaGT@users.noreply.github.com>
2023-09-22 15:23:11 -0300

    arreglamos el problma de los arrays

--- web/src/pages/perfil.js
+++ web/src/pages/perfil.js
@@ -14,25 +14,28 @@ import React, { useState, createElement } from "react";
 const pacientes = [
   {
     datosPaciente: {
+      id: 1,
       Nombre: "John",
       Apellido: "Doe",
-      edad: 30,
+      Edad: 30,
       Mail: "john@example.com"
     }
   },
   {
     datosPaciente: {
+      id: 2,
       Nombre: "Jane",
       Apellido: "Smith",
-      edad: 25,
+      Edad: 25,
       Mail: "jane@example.com"
     }
   },
   {
     datosPaciente: {
+      id: 3,
       Nombre: "Alice",
       Apellido: "Johnson",
-      edad: 40,
+      Edad: 40,
       Mail: "alice@example.com"
     }
   }
@@ -55,15 +58,26 @@ function createDivsFromArray(Pacientes) {
     container.appendChild(newDiv);
   }
 }*/
-function crearDiv(){
+
+
+function crearDiv(id) {
   var div = document.createElement('div');
   div.id = "container";
-  div.textContent = {pacientes};
-  div.classList.add('bg-yellow-400', 'card', 'p-4', 'm-4', 'w-1/4');
-  //div.innerHTML = document.getElementById('container');
-  document.body.appendChild(div);
-  
 
+  // Use filter to find the patient with the matching id
+  const paciente = pacientes[id];
+  const datos = paciente.datosPaciente;
+  console.log({paciente:paciente, pacientes:pacientes})
+
+  // Check if a matching patient was found
+  if (paciente) {
+    div.textContent = "Nombre: " + datos.Nombre + " \nApellido: " + datos.Apellido + " \nEdad: " + datos.Edad;
+  } else {
+    div.textContent = "Paciente no encontrado";
+  }
+
+  div.classList.add('bg-red-400', 'card', 'p-4', 'm-4', 'w-1/4');
+  document.body.appendChild(div);
 }
 /*const listPacientes = pacientes.map((datosPaciente)=>
 <li key={datosPaciente.Nombre}>
@@ -71,10 +85,12 @@ function crearDiv(){
 </li>);
 */
 const showPacientes = () => {
-pacientes.forEach(datosPaciente => {
-    
-    console.log({datosPaciente});
-    crearDiv();
+  var i = 0;
+pacientes.forEach(pacienteMostrar => {
+    //console.log({pacienteMostrar});
+    crearDiv(i);
+
+    i += 1;
   });
 }
 
@@ -86,28 +102,28 @@ const handleClick = () =>{
 
 export default function Perfil() {
   console.log("render app");
-  console.log(pacientes);
+  // console.log(pacientes);
   return (
     <main>
       <Navbar />
       {/* <h1></h1> */}
       <div className="flex w-full p-4">
         <div className="grid  flex-grow card bg-base-300 rounded-box place-items-center">
-          <h1 className="card card-title">Perfil</h1>
-          <div>
-            <button className="btn btn-secondary-300" >Ver Pacientes</button>
+          <h1 className="card card-title p-8 h-full">Perfil</h1>
+          <div className="p-4">
+            <button className="btn btn-secondary-300 btn-wide " >Ver Pacientes</button>
             
           </div>
         </div>
 
         <div className="divider divider-horizontal"></div>
         <div className="grid  flex-grow card bg-base-300 rounded-box place-items-center">
-          <h1 className="card card-title p-4">Historial</h1>
-          <button className="btn btn-secondary-300" onClick={showPacientes}>
+          <h1 className="card card-title p-8 h-full">Historial</h1>
+          <div className="p-4">
+          <button className="btn btn-secondary-300  btn-wide " onClick={showPacientes}>
             Mostrar Pacientes
           </button>
-          <button onClick={crearDiv}>Click me</button>
-         <div id="container">container</div>
+         </div>
 
         </div>
       </div>
