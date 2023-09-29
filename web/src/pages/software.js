3b6b3705c8bac71545a103a62693c577adab2773
JulietaGT <108838724+JulietaGT@users.noreply.github.com>
2023-09-29 15:57:00 -0300

    lo hice una tabla

--- web/src/pages/software.js
+++ web/src/pages/software.js
@@ -4,10 +4,10 @@ const pacientes = [
   {
     datosPaciente: {
       id: 1,
-      Nombre: "John",
-      Apellido: "Doe",
-      Edad: 30,
-      Mail: "john@example.com",
+      Nombre: "He",
+      Apellido: "Man",
+      Edad: 20,
+      Mail: "Heman@example.com",
     },
   },
   {
@@ -22,10 +22,10 @@ const pacientes = [
   {
     datosPaciente: {
       id: 3,
-      Nombre: "Alice",
-      Apellido: "Johnson",
-      Edad: 40,
-      Mail: "alice@example.com",
+      Nombre: "Taylor",
+      Apellido: "Swift",
+      Edad:34,
+      Mail: "swiftie@example.com",
     },
   },
   {
@@ -35,6 +35,15 @@ const pacientes = [
       Apellido: "Jackson",
       Edad: 43,
       Mail: "janice@example.com",
+    },
+  },
+  {
+    datosPaciente: {
+      id: 5,
+      Nombre: "Ron",
+      Apellido: "Weasley",
+      Edad: 17,
+      Mail: "weasley@example.com",
     },
   },
 ];
@@ -87,49 +96,46 @@ export default function Software() {
   return (
     <main>
       <Navbar />
-      <div className="w-full grid grid-flow-row auto-rows-max">
-        <div id="container" className="bg-red-300 h-fit flex">
-          {/* <button
-            className="btn btn-secondary-300 btn-md self-center"
-            onClick={showPacientes}
-          >
-            Mostrar Pacientes
-          </button> */}
+      <div className="flex">
 
-          <div class="dropdown dropdown-hover dropdown-left dropdown-bottom">
-            <button tabindex="0" className="btn">
-              Hover
-            </button>
-            <ul
-              tabindex="0"
-              class="dropdown-content  z-[1] menu p-2  bg-red-500 rounded-box w-52"
-            >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Item 2</a>
-              </li>
-            </ul>
-          </div>
+        {/* Avatar o Perfil*/}
+      <div className="h-sreen grow bg-pink-400  w-3/12">
+       <h1 className="text-blue-900 text-2xl font-extrabold p-4">Pacientes</h1>
+       <select className="select w-full max-w-xs">
+        <option>A-Z</option>
+        <option>ID</option>
+        <option>A-Z</option>
+       </select>
         </div>
-          <div className="bg-red">
-            {pacientes.map((p) => {
-              return (
-                <div>
-              <thread> {/*TABLA*/}
-                <th>Nombre</th><br/>
+
+        <div className="overflow-x-auto flex w-full">
+          <div className="table p-4  w-full  ">
+          <thread className="text-xl"> {/*TABLA*/}
+                <th>Nombre</th>
                 <th>Mail</th>
                 </thread> 
 
-                <tbody>   
-
+            {pacientes.map((p) => {
+              return (
+                <div>              
+                <tbody className=" flex ">   
+                <tr className="hover w-full">
+                  <td>
+                    <div tabIndex="0" className="collapse bg-primary-200">
+                    <span className="collapse-title text-md font-medium">{p.datosPaciente.id} {p.datosPaciente.Nombre}  {p.datosPaciente.Apellido}  {p.datosPaciente.Mail}</span>
+                    <div className="collapse-content">
+                      <button className="btn btn-sm">Editar</button>
+                    </div>
+                    </div>
+                  </td>
+                </tr>
               {/* <div className="bg-blue-600 table-auto w-full flex ">
               <span> {p.datosPaciente.Nombre}  {p.datosPaciente.Apellido}  {p.datosPaciente.Mail}</span>
               </div> */}
               </tbody>
               </div>);
             })}
+          </div>
           </div>
       </div>
     </main>
