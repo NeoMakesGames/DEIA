f43da9a4e53ae1d6e819b8652c80f5e820a5bb54
JulietaGT <108838724+JulietaGT@users.noreply.github.com>
2023-10-09 17:24:03 -0300

    cambie el select x un search

--- web/src/pages/software.js
+++ web/src/pages/software.js
@@ -201,70 +201,81 @@ const pacientes = [
 //   });
 // };
 
-export default function Software() {
-  const [data, setData] = useState(pacientes.datosPaciente);
-  const sortData = () => {
-    const sortedData = [...data];
-    sortedData.sort((a, b) => a[0].localeCompare(b[0]));
-    setData(sortedData);
-  };
+ export default function Software() {
+  
+   const data = pacientes;
 
+  
+    const [message, setmessage] = useState('');
+
+      const handleChange = (event) => {
+        console.log(message);
+        alert('el nombre es' +{message});
+      }
+
+      
+   if (handleChange() != null) {
+       data.filter((h) => h.include(handleChange()));
+      console.log("hiciste una busqueda");
+   }
+   else {
+     console.log("no buscaste nada");
+   }
+  
   return (
     <main>
       <Navbar />
       <div className="flex">
         {/* Avatar o Perfil*/}
-        <div className="h-sreen grow bg-base-300  w-3/12">
+        <div className="h-screen grow bg-base-300  w-3/12">
           <h1 className="text-blue-900 text-2xl font-bold p-4">Pacientes</h1>
-
-          <button className="btn" onClick={sortData}>
-            Sort Data
-          </button>
-
-          {/* <select className="select w-1/2 justify-self-center max-w-xs">
-            <option>ID</option>
-            <option>Z-A</option>
-          </select> */}
+          {/* SEARCH */}
+          <input type="text" id="message" name="message" onChange={(e) => setmessage(e.target.value)} value={message} placeholder="Nombre del paciente" className="input input-bordered w-full max-w-xs"/>
+          <h1>{message}</h1>
+        
         </div>
 
-        <div className="overflow-x-auto flex w-full bg-neutral-100">
-          <div className="table p-4  w-full  ">
-            <table>
+        <div className="overflow-x-auto flex w-screen h-screen bg-neutral-100">
+          <div className="p-4">
+            <table className="table">
               <thead className="text-xl">
                 <th>Nombre</th>
                 <th>Mail</th>
               </thead>
 
-              <tbody className=" flex ">
+              <tbody className=" flex table-auto">
                 {data &&
                   data.map((d) => {
+                    console.log("la lista se rendriza")
                     return (
                       <div>
-                        <tr className="hover w-full">
+                        <tr className="hover w-full table-auto">
                           <td>
-                            <div tabIndex="0" className="collapse">
-                              <span className="collapse-title text-md font-medium">
-                                {" "}
-                                {d.Nombre} {d.Apellido} {d.Mail}
-                              </span>
+                              <span>{d.datosPaciente.Nombre}{d.datosPaciente.Apellido}{d.datosPaciente.Mail}</span>
                               <div className="collapse-content">
                                 <button className="btn btn-sm">Editar</button>
                               </div>
-                            </div>
                           </td>
-                        </tr>
-                        {/* {Data.map((person, index) => (
-                          <tr key={index}>
-                            <td>{person[0]}</td>
-                            <td>{person[1]}</td>
-                          </tr>
-                        ))} */}
-                        {/* <div className="bg-blue-600 table-auto w-full flex ">
-                      <span> {p.datosPaciente.Nombre}  {p.datosPaciente.Apellido}  {p.datosPaciente.Mail}</span>
-                      </div> */}
-                      </div>
-                    );
-                  })}
+                          
+                        </tr> 
+                        {/* {data.map ((d) => {
+                          console.log("lista renderizada");
+                          return(
+                         <tr className="p-4 table-auto hover w-full flex ">
+                           <td>
+                      <span> {d.datosPaciente.Nombre}  {d.datosPaciente.Apellido}  {d.datosPaciente.Mail}
+                      <div className="collapse-content">
+                                <button className="btn btn-sm">Editar</button>
+                              </div>
+                      </span>
+                      </td>
+                      </tr> 
+
+                          )
+                        })} */}
+                        </div>
+                        )})}
+                  
               </tbody>
             </table>
           </div>
