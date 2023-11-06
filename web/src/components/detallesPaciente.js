825985d96aca886983a88b978cc647ec904678c7
JulietaGT <108838724+JulietaGT@users.noreply.github.com>
2023-11-06 09:15:59 -0300

    agregue el diagnostico

--- web/src/components/detallesPaciente.js
+++ web/src/components/detallesPaciente.js
@@ -1,20 +1,14 @@
-
-export default function DetallesPaciente({id}){
-
-    return(
-        <main>
-
-        <section>
-            <div className=" my-8 flex items-center justify-center gap-4  flex-col ">
-            <h1 className="my-8 font-bold text-[35px]">Paciente</h1>
-            <div>
-             <h1>Nombre: {id}</h1> 
-             <h1>Fecha de Nacimiento: {id}</h1> 
-             <h1>Genero: {id}</h1> 
-            </div>
-            </div>
-            
-        </section>
-        </main>
-    )
+export default function DetallesPaciente({ id }) {
+  return (
+    <main>
+      <section>
+        
+          <div className=" my-4 flex justify-items-center justify-center gap-40 w-full">
+            <h1>Nombre: {id}</h1>
+            <h1>Fecha de Nacimiento: {id}</h1>
+            <h1>Genero: {id}</h1>
+          </div>
+      </section>
+    </main>
+  );
 }
