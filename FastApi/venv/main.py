6f00b310ac05ed1fc4cac0d1e4871108ab0400da
BEHREND LUCAS <lucasbehrend2006@gmail.com>
2023-11-09 12:08:14 -0300

    cambie el modelo

--- FastApi/venv/main.py
+++ FastApi/venv/main.py
@@ -12,8 +12,7 @@ print("ok")
 @app.post("/predict")
 async def root(data: InputData):
     print("yujuuuuu")
-    ruta_modelo_hdf5 = "best_model_NO-O.h5"
-    #datos_de_ejemplo =[[3.32, 77, 4.07, 80]]
+    ruta_modelo_hdf5 = "modelo_no-o.h5"
     model = ModeloIA(ruta_modelo_hdf5)
     # if not data.FEV1Value or not data.FEV1Pred or not data.FVCValue or not data.FVCPred:
     #     raise HTTPException(status_code=422, detail = "No se completaron todos los datos.")
